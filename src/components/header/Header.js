4e05b0f2b7148d04c9e68abbc1abe54d3407ae16
Marcia <marcia.dasilva1410@gmail.com>
2018-08-09 17:53:56 -0300

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import FacebookLogo from "./images/facebook.png";
 import InstagramLogo from "./images/instagram.png";
 import TwitterLogo from "./images/twitter.png";
+import CodepenLogo from "./images/codepen.png";
+import GithubLogo from "./images/github.png";
+import LinkedinLogo from "./images/linkedin.png";
 
 import "./Header.css";
 
@@ -46,6 +49,18 @@ const Header = () => {
           </a>
           <a href="https://twitter.com/mds_marcia" className="header__icon">
             <img src={TwitterLogo} alt="Twitter logo" />
+          </a>
+          <a href="https://codepen.io/marciadasilva/" className="header__icon">
+            <img src={CodepenLogo} alt="Codepen logo" />
+          </a>
+          <a href="https://github.com/marciadasilva" className="header__icon">
+            <img src={GithubLogo} alt="Github logo" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/marcia-silva-068b4666/"
+            className="header__icon"
+          >
+            <img src={LinkedinLogo} alt="Linkedin logo" />
           </a>
         </div>
       </nav>

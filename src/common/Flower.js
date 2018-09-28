11bc2851a6f2672e364bfa152dd1b83645657541
Marcia <marcia.dasilva1410@gmail.com>
2018-09-28 10:14:53 -0300

    added flower component

---
+++ src/common/Flower.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Flower.css";
+
+const Flower = () => (
+  <div className="flower">
+    <div className="big-petal big-petal1" />
+    <div className="big-petal big-petal2" />
+    <div className="big-petal big-petal3" />
+    <div className="big-petal big-petal4" />
+    <div className="small-petal small-petal1" />
+    <div className="small-petal small-petal2" />
+    <div className="small-petal small-petal3" />
+    <div className="small-petal small-petal4" />
+    <div className="small-petal small-petal5" />
+    <div className="small-petal small-petal6" />
+    <div className="small-petal small-petal7" />
+    <div className="small-petal small-petal8" />
+  </div>
+);
+
+export default Flower;

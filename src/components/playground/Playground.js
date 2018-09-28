4f32e185c4428164526ae099403b078dfc9d2d6e
Marcia <marcia.dasilva1410@gmail.com>
2018-09-28 08:55:14 -0300

    inserted new pens

--- src/components/playground/Playground.js
+++ src/components/playground/Playground.js
@@ -15,6 +15,12 @@ const Playground = () => (
         all my pens! <span> 👀</span>
       </h2>
       <div className="pens">
+        <h3 className="pen-title">Flower grid css</h3>
+        <div className="pen">
+          <Codepen hash="NBQjYy" user="marciadasilva" height="500" />
+        </div>
+      </div>
+      <div className="pens">
         <h3 className="pen-title">Rotating words</h3>
         <div className="pen">
           <Codepen hash="rrjBow" user="marciadasilva" height="500" />
@@ -24,6 +30,24 @@ const Playground = () => (
         <h3 className="pen-title">Playing with hue-rotating</h3>
         <div className="pen">
           <Codepen hash="WKGNEM" user="marciadasilva" height="500" />
+        </div>
+      </div>
+      <div className="pens">
+        <h3 className="pen-title">CSS curtain reveal content</h3>
+        <div className="pen">
+          <Codepen hash="KxGprP" user="marciadasilva" height="500" />
+        </div>
+      </div>
+      <div className="pens">
+        <h3 className="pen-title">Hover image</h3>
+        <div className="pen">
+          <Codepen hash="RYdBqL" user="marciadasilva" height="500" />
+        </div>
+      </div>
+      <div className="pens">
+        <h3 className="pen-title">Form show password</h3>
+        <div className="pen">
+          <Codepen hash="jpjwvb" user="marciadasilva" height="500" />
         </div>
       </div>
       <div className="pens">

ed2fd341130e3715943919041fbcff8b68e78737
Igor Haugg <igor-haugg@hotmail.com>
2019-10-01 22:04:30 -0400

    version updated and npm audit fix

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import React, { Component } from "react";
-import Loadable from "react-loadable";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Loadable from 'react-loadable';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Spinner from "./common/Spinner";
+import Spinner from './common/Spinner';
 
 // Loading component
 const Loading = () => (
-  <div className="full-centralize full-screen">
-    <Spinner />
-  </div>
+	<div className='full-centralize full-screen'>
+		<Spinner />
+	</div>
 );
 
 // Components
 const Home = Loadable({
-  loader: () => import("./components/home/Home"),
-  loading: () => <Loading />
+	loader  : () => import('./components/home/Home'),
+	loading : () => <Loading />
 });
 
 const Works = Loadable({
-  loader: () => import("./components/works/Works"),
-  loading: () => <Loading />
+	loader  : () => import('./components/works/Works'),
+	loading : () => <Loading />
 });
 
 const Playground = Loadable({
-  loader: () => import("./components/playground/Playground"),
-  loading: () => <Loading />
+	loader  : () => import('./components/playground/Playground'),
+	loading : () => <Loading />
 });
 
 const Contact = Loadable({
-  loader: () => import("./components/contact/Contact"),
-  loading: () => <Loading />
+	loader  : () => import('./components/contact/Contact'),
+	loading : () => <Loading />
 });
 
 const About = Loadable({
-  loader: () => import("./components/about/About"),
-  loading: () => <Loading />
+	loader  : () => import('./components/about/About'),
+	loading : () => <Loading />
 });
 
-class App extends Component {
-  render() {
-    return (
-      <Router basename="/portifolio">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/recent-works" component={Works} />
-          <Route exact path="/playground" component={Playground} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => (
+	<Router basename='/portifolio'>
+		<Switch>
+			<Route exact path='/' component={Home} />
+			<Route exact path='/recent-works' component={Works} />
+			<Route exact path='/playground' component={Playground} />
+			<Route exact path='/about' component={About} />
+			<Route exact path='/contact' component={Contact} />
+		</Switch>
+	</Router>
+);
 
 export default App;
